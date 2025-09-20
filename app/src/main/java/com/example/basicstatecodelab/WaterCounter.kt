860771e6b414a877f4280d92d4b551fbea4feef0
ARTEM <arte.iotenorio@gmail.com>
2025-09-20 01:25:12 -0600

    es el punto 5 no se que cambio

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,21 +1,18 @@
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count by remember { mutableStateOf(0) }
-
     Column(modifier = modifier.padding(16.dp)) {
-        Text(text = "You've had $count glasses.")
-        Button(
-            onClick = { count++ },
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
+        var count by remember { mutableStateOf(0) }
+
+        Text("You've had $count glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
