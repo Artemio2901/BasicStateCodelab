7dbb5d0bb71b73309c41c14d6b7cc99207557b73
ARTEM <arte.iotenorio@gmail.com>
2025-09-21 17:58:06 -0600

    paso 7

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -3,18 +3,32 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
+    var count by remember { mutableStateOf(0) }
+
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
         if (count > 0) {
             Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
-            Text("Add one")
+
+        Row(modifier = Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = { count++ },
+                enabled = count < 10
+            ) {
+                Text("Add one")
+            }
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Button(
+                onClick = { count = 0 },
+                enabled = count > 0
+            ) {
+                Text("Clear water count")
+            }
         }
     }
 }
